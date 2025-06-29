8acaabff3fd287f44ac5ec6417b259053de1c4bb
Ken Earl <kenearl26@gmail.com>
2025-06-29 22:54:56 +0800

    add project three named Roll a Dice

---
+++ Roll_a_Dice/rollDice.js
@@ -0,0 +1,18 @@
+function rollDice(){
+    const numOfDice = document.getElementById("numOfDice").value;
+    const resultValues = document.getElementById("resultValues");
+    const imageValues = document.getElementById("imageValues");
+    const values = [];
+    const images = [];
+    if(numOfDice == 0){
+        resultValues.textContent = "no dice rolled";
+        return;
+    }
+    for(let i = 0; i < numOfDice; i++){
+        const value = Math.floor(Math.random() * 6 + 1);
+        values.push(value);
+        images.push(`img src = Dice-images/${value}.jpg`)
+    }
+    resultValues.textContent = `dice: ${values.join(`, `)}`;
+    imageValues.innerHTML = images.join(" ");
+}
